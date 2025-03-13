f9854910ba62343a1f8a15786bd5ba89a43472c3
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-03-13 13:10:18 +0300

    Add files via upload

---
+++ TravelBook/TravelBookTests/TravelBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBookTests.swift
+//  TravelBookTests
+//
+//  Created by Berkan Aydın on 28.12.2024.
+//
+
+import Testing
+@testable import TravelBook
+
+struct TravelBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
